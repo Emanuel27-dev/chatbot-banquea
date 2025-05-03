08a65649e9015cd2f7d34092076fdcc6826d83b2
Enrique <xyxyxy27.79@gmail.com>
2025-05-03 18:16:40 -0500

    este mensaje es para cambiar el titulo  por documentos

--- frontend-react/src/components/Titles.jsx
+++ frontend-react/src/components/Titles.jsx
@@ -17,7 +17,7 @@ export const Titles = () => {
   return (
     <article className={style.article}>
       <header className={style.header}>
-        <h1 className={style.headerTitle}>Titulos</h1>
+        <h1 className={style.headerTitle}>DOCUMENTOS 👽👽</h1>
       </header>
       <div className={style.containerTitles}>
         {titles.map((title) => (
